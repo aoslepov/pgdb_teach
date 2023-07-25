f8eed864c4965d10a91430faa97cd877b0ce4bc4
Alexander <ao.slepov@gmail.com>
2023-07-25 19:41:08 +0300

    playbook

---
+++ project/yc/playbook_start/playbook.sh
@@ -0,0 +1,1 @@
+export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u aslepov -i /home/aslepov/aslepov_repo/pgdb_teach/project/yc/playbook_start/hosts playbook.yml -e "permit_root_login=yes" -b #--ask-vault-pass
